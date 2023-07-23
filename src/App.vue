a11c4f02940612e7fa59a15c7e3c8ff1b9689bd5
rvdegroen <suiyichiro@hotmail.com>
2023-07-23 20:21:56 +0200

    set up basic routing and removed some template code

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- replaces component with HomeView -->
+  <header>
+    <nav>
+      <RouterLink to="/">ACNH Finder</RouterLink>
+      <RouterLink to="/todolist">Todo List</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
